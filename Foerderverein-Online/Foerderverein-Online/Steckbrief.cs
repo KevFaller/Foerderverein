23ecdcf3f8c6d65575098f9ba55fbdca239420de
Kevin Faller <kevinfaller@imac-von-kevin.speedport.ip>
2023-10-31 17:20:16 +0100

    Arbeitsstand

---
+++ Foerderverein-Online/Foerderverein-Online/Steckbrief.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Foerderverein_Online
+{
+	public class Steckbrief
+	{
+            public string Name { get; set; }
+            public string BildPfad { get; set; }
+            public string Position { get; set; }
+            public string UeberMich { get; set; }
+        
+
+    }
+}
+
